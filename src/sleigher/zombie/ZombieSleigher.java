c2363fdc053b87106ef1a7222d1dc95de1077b7d
Jack Dahms <jack.dahms@gmail.com>
2015-10-06 10:34:45 -0500

    create jframe

--- src/sleigher/zombie/ZombieSleigher.java
+++ src/sleigher/zombie/ZombieSleigher.java
@@ -1,9 +1,25 @@
 package sleigher.zombie;
 
-public class ZombieSleigher {
+import javax.swing.JFrame;
+
+public class ZombieSleigher { //we want to make this a canvas and use bufferstrategy, not jpanel
+
+    public static int WIDTH = 600;
+    public static int HEIGHT = 600;
 
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Santa Sleigher");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setLocationRelativeTo(null);
+        
+        //add ZombieSleigher to jframe
+        
+        frame.setVisible(true);
+    }
     
+    public ZombieSleigher() {
+        
     }
 
 }
