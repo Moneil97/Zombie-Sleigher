bcbed1852fd307e61f6fc6a90d6ea8f83c70fb35
jack <jack.dahms@gmail.com>
2015-10-08 08:31:50 -0500

    added update method

--- src/sleigher/zombie/Santa.java
+++ src/sleigher/zombie/Santa.java
@@ -1,8 +1,39 @@
 package sleigher.zombie;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 public class Santa {
 	
 	float x, y;
 	float xs, ys;
+	
+	boolean up, down, left, right;
+	
+	BufferedImage image;
+	
+	public Santa() {
+		//TODO read buffered image
+	}
+	
+	/**
+	 * feature creep:
+	 * acceleration
+	 * max speed (right now diagonals are faster)
+	 */
+	public void update() {
+		if (up) y -= ys;
+		if (down) y += ys;
+		if (left) x -= xs;
+		if (right) x += xs;
+	}
+	
+	/**
+	 * feature creep:
+	 * sleigh slightly turns left or right
+	 */
+	public void render(Graphics2D g, float delta) {
+		
+	}
 
 }
